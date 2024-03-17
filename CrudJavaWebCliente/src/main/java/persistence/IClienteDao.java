8cf9eb99822993b81ad507d8fb7e1b1948a1a4ab
Jonathan Custodio <126304158+JonathanOliveiraCustodio@users.noreply.github.com>
2024-03-17 00:22:32 -0300

    Add files via upload

---
+++ CrudJavaWebCliente/src/main/java/persistence/IClienteDao.java
@@ -0,0 +1,12 @@
+package persistence;
+
+import java.sql.SQLException;
+
+import model.Cliente;
+
+public interface IClienteDao {
+	
+	public String iudCliente(String acao, Cliente p) throws SQLException, ClassNotFoundException;
+	
+
+}
